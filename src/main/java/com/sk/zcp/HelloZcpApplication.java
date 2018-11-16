b4c3065a55293fb83e2148bd61002524812224a4
Jigyoomin <earth1223@sk.com>
2018-11-16 14:44:19 +0900

    test

--- src/main/java/com/sk/zcp/HelloZcpApplication.java
+++ src/main/java/com/sk/zcp/HelloZcpApplication.java
@@ -1,20 +1,34 @@
 package com.sk.zcp;
 
+import java.io.BufferedWriter;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.net.InetSocketAddress;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.BiConsumer;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.sk.zcp"})
 public class HelloZcpApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(HelloZcpApplication.class);
+	
 	public static void main(String[] args) {
 		SpringApplication.run(HelloZcpApplication.class, args);
 	}
@@ -23,10 +37,47 @@ public class HelloZcpApplication {
 	public String home() {
 		return "Hello ZCP";
 	}
+	
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	@GetMapping("/info") 
+	public String info(@RequestHeader Map header, HttpServletRequest req) {
+		StringBuffer sb = new StringBuffer();
+		sb.append("<b>HTTP HEADER</b><br/>");
+		header.forEach((key, value) -> {sb.append(String.format("%s : %s <br/>", key, value));});
+		sb.append("<b>Remote</b><br/>");
+		sb.append("getServerName : ").append(req.getServerName()).append("<br/>");
+		sb.append("getServerPort : ").append(req.getServerPort()).append("<br/>");
+		sb.append("getContextPath : ").append(req.getContextPath()).append("<br/>");
+		sb.append("getServletPath : ").append(req.getServletPath()).append("<br/>");
+		sb.append("getRequestURL : ").append(req.getRequestURL().toString()).append("<br/>");
+		sb.append("getRemoteAddr : ").append(req.getRemoteAddr()).append("<br/>");
+		sb.append("getRemoteHost : ").append(req.getRemoteHost()).append("<br/>");
+		sb.append("getRemoteUser : ").append(req.getRemoteUser()).append("<br/>");
+		sb.append("getRemotePort : ").append(req.getRemotePort()).append("<br/>");
+				
+		return sb.toString();
+	}
   
-  	@GetMapping("/errorservice") 
+  	@GetMapping("/printErrorLog") 
 	public String error() throws Exception{
-		throw new Exception("Error service");
+  		Exception exception = new Exception("Error service");
+  		StringWriter sw = new StringWriter();
+  		BufferedWriter bw = new BufferedWriter(sw);
+  		PrintWriter pw = new PrintWriter(bw);
+  		
+  		exception.printStackTrace(pw);
+  		pw.flush();
+  		String stacktrace = sw.toString();
+  		stacktrace = stacktrace.replaceAll("\n", "<br/>");
+  		stacktrace = stacktrace.replaceAll("\t", "&nbsp;&nbsp;&nbsp;&nbsp;");
+  		pw.close();
+		logger.error("print stacktrace", exception);
+		return "log --------<br>" + stacktrace;
+	}
+  	
+  	@GetMapping("/404") 
+	public void notFound(HttpServletResponse httpResponse) throws Exception{
+  		httpResponse.sendRedirect("/");
 	}
   
   	@SuppressWarnings("static-access")
---
+++ src/main/java/com/sk/zcp/WebMvcConfiguration.java
@@ -0,0 +1,34 @@
+package com.sk.zcp;
+
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.web.servlet.ErrorPage;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class WebMvcConfiguration extends WebMvcConfigurerAdapter {
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/notFound").setViewName("forward:/index.html");
+	}
+
+	@Bean
+	public EmbeddedServletContainerCustomizer containerCustomizer() {
+
+		return new EmbeddedServletContainerCustomizer() {
+			@Override
+			public void customize(ConfigurableEmbeddedServletContainer container) {
+				ErrorPage error401Page = new ErrorPage(HttpStatus.UNAUTHORIZED, "/401.html");
+				ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/404");
+				ErrorPage error500Page = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/500.html");
+
+				container.addErrorPages(error401Page, error404Page, error500Page);
+			}
+		};
+	}
+}
