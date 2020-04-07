c4c2de8b3c495fccbdbd918a9eb9347bff54f550
zodiac12k <zodiac12k@gmail.com>
2020-04-07 11:14:05 +0900

    Update spring boot parent version to 2.2.1.RELEASE

--- src/main/java/com/sk/zcp/WebMvcConfiguration.java
+++ src/main/java/com/sk/zcp/WebMvcConfiguration.java
@@ -1,19 +1,18 @@
 package com.sk.zcp;
 
-import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
-import org.springframework.boot.web.servlet.ErrorPage;
+import com.sk.zcp.log.MdcLoggingFilter;
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-import com.sk.zcp.log.MdcLoggingFilter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebMvcConfiguration extends WebMvcConfigurerAdapter {
+public class WebMvcConfiguration implements WebMvcConfigurer {
 
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
@@ -21,19 +20,33 @@ public class WebMvcConfiguration extends WebMvcConfigurerAdapter {
 	}
 
 	@Bean
-	public EmbeddedServletContainerCustomizer containerCustomizer() {
-
-		return new EmbeddedServletContainerCustomizer() {
+	public WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> factoryCustomizer() {
+		return new WebServerFactoryCustomizer<ConfigurableServletWebServerFactory>() {
 			@Override
-			public void customize(ConfigurableEmbeddedServletContainer container) {
+			public void customize(ConfigurableServletWebServerFactory factory) {
 				ErrorPage error401Page = new ErrorPage(HttpStatus.UNAUTHORIZED, "/401.html");
 				ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/404");
 				ErrorPage error500Page = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/500.html");
 
-				container.addErrorPages(error401Page, error404Page, error500Page);
+				factory.addErrorPages(error401Page, error404Page, error500Page);
 			}
 		};
 	}
+
+//	@Bean
+//	public EmbeddedServletContainerCustomizer containerCustomizer() {
+//
+//		return new EmbeddedServletContainerCustomizer() {
+//			@Override
+//			public void customize(ConfigurableEmbeddedServletContainer container) {
+//				ErrorPage error401Page = new ErrorPage(HttpStatus.UNAUTHORIZED, "/401.html");
+//				ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/404");
+//				ErrorPage error500Page = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/500.html");
+//
+//				container.addErrorPages(error401Page, error404Page, error500Page);
+//			}
+//		};
+//	}
 	
 	@Bean
 	public FilterRegistrationBean loggingFilter(){
